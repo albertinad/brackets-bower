3f595521d549ebe5d3c34b08cb8b73e0be720853
Albertina Durante <albertina.durante@intel.com>
2014-12-18 17:55:31 -0300

    Fixing tests: - Migrate NodeConnection to NodeDomain - Update bower domain APIs calls

--- unittests.js
+++ unittests.js
@@ -1,24 +1,24 @@
 /*
  * Copyright (c) 2013 Adobe Systems Incorporated. All rights reserved.
- *  
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"), 
- * to deal in the Software without restriction, including without limitation 
- * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
- * and/or sell copies of the Software, and to permit persons to whom the 
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following conditions:
- *  
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *  
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
- * 
+ *
  */
 
 
@@ -27,10 +27,10 @@
 
 define(function (require, exports, module) {
     "use strict";
-    
-    var NodeConnection   = brackets.getModule("utils/NodeConnection"),
+
+    var NodeDomain       = brackets.getModule("utils/NodeDomain"),
         FileUtils        = brackets.getModule("file/FileUtils"),
-        NativeFileSystem = brackets.getModule("file/NativeFileSystem").NativeFileSystem,
+        FileSystem       = brackets.getModule("filesystem/FileSystem"),
         ExtensionUtils   = brackets.getModule("utils/ExtensionUtils"),
         SpecRunnerUtils  = brackets.getModule("spec/SpecRunnerUtils"),
         Async            = brackets.getModule("utils/Async");
@@ -39,55 +39,31 @@ define(function (require, exports, module) {
     var testFolder      = FileUtils.getNativeModuleDirectoryPath(module) + "/tmp-test";
 
     describe("bower-install", function () {
-        
+
         // Unit tests for the underlying node server.
         describe("BowerDomain", function () {
-            var nodeConnection,
+            var bowerDomain,
+                fileUtilsDomain,
                 testBrackets,
-                ProjectManager;
-            
+                ProjectManager,
+                emptyConfig = {};
+
             beforeEach(function () {
                 runs(function () {
-                    var nodePromise = new $.Deferred();
-                    nodeConnection = new NodeConnection();
-                    nodeConnection.connect(true).then(function () {
-                        nodeConnection.loadDomains(
-                            [ExtensionUtils.getModulePath(module, "node/FileUtilsDomain")],
-                            true
-                        ).then(function () {
-                            nodePromise.resolve();
-                        });
-                    });
-                    waitsForDone(nodePromise, "connecting to node server");
-                });
-                
-                runs(function () {
-                    var domainPromise = new $.Deferred(),
-                        retries = 0;
+                    var bowerDomainPath = ExtensionUtils.getModulePath(module, "/node/BowerDomain"),
+                        fileUtilsDomainPath = ExtensionUtils.getModulePath(module, "/node/FileUtilsDomain");
 
-                    function waitForDomain() {
-                        if (nodeConnection.domains.bower) {
-                            domainPromise.resolve();
-                        } else {
-                            retries++;
-                            if (retries >= 5) {
-                                domainPromise.reject();
-                            } else {
-                                setTimeout(waitForDomain, 100);
-                            }
-                        }
-                    }
-                    
-                    waitForDomain();
-                    waitsForDone(domainPromise, "waiting for Bower domain to load");
+                    bowerDomain = new NodeDomain("bower", bowerDomainPath);
+                    fileUtilsDomain = new NodeDomain("fileUtils", fileUtilsDomainPath);
+                    //waitsForDone(nodePromise, "connecting to node server");
                 });
-                
+
                 runs(function () {
                     var folderPromise = new $.Deferred();
                     SpecRunnerUtils.createTestWindowAndRun(this, function (testWindow) {
                         testBrackets = testWindow.brackets;
                         ProjectManager = testBrackets.test.ProjectManager;
-                        
+
                         testBrackets.fs.makedir(testFolder, 0, function (err) {
                             if (err === testBrackets.fs.NO_ERROR) {
                                 ProjectManager.openProject(testFolder)
@@ -101,48 +77,52 @@ define(function (require, exports, module) {
                     waitsForDone(folderPromise, "waiting for test folder to be created");
                 });
             });
-            
+
             afterEach(function () {
                 runs(function () {
                     SpecRunnerUtils.closeTestWindow();
-                    waitsForDone(nodeConnection.domains.bower_test_fileutils.deleteRecursive(testFolder), "deleting test folder");
+
+                    fileUtilsDomain.exec("deleteRecursive", testFolder);
                 });
-                
+
                 runs(function () {
-                    nodeConnection.disconnect();
-                    nodeConnection = null;
+                    bowerDomain = null;
+                    fileUtilsDomain = null;
                 });
             });
-            
+
             it("should return a list of package names", function () {
                 var serverInfo, packages, i;
+
                 runs(function () {
-                    nodeConnection.domains.bower.getPackages()
+                    bowerDomain.exec("getPackages", emptyConfig)
                         .done(function (data) {
                             packages = data;
                         });
                 });
-                
-                waitsFor(function () { return packages; }, "waiting for bower package list", 10000);
-                
+
+                waitsFor(function () { return packages; }, "waiting for bower package list", 100000);
+
                 runs(function () {
                     expect(Array.isArray(packages)).toBe(true);
                     expect(packages.length).toBeGreaterThan(0);
+
                     for (i = 0; i < packages.length; i++) {
                         expect(typeof packages[i].name).toBe("string");
                         expect(typeof packages[i].url).toBe("string");
                     }
                 });
             });
-            
+
             it("should install jquery", function () {
                 runs(function () {
-                    waitsForDone(nodeConnection.domains.bower.installPackage(testFolder, "jquery"),
+                    waitsForDone(bowerDomain.exec("installPackage", testFolder, "jquery", emptyConfig),
                                  "installing jquery", 10000);
                 });
                 runs(function () {
-                    waitsForDone(FileUtils.readAsText(new NativeFileSystem.FileEntry(testFolder + "/bower_components/jquery/jquery.js")),
-                                 "reading installed jquery");
+                    var file = FileSystem.getFileForPath(testFolder + "/bower_components/jquery/dist/jquery.min.js");
+
+                    waitsForDone(FileUtils.readAsText(file), "reading installed jquery");
                 });
             });
         });
