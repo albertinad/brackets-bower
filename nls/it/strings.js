251c72c72bca697bfdadba294d9e9013a2b37ce8
Denis R <student@alice.it>
2015-04-05 17:07:30 +0200

    Create strings.js

---
+++ nls/it/strings.js
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2014 Narciso Jaramillo. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define */
+
+define({
+    "TITLE_BOWER": "Bower",
+    "TITLE_WARNING": "Avvertimenti Bower",
+    "TITLE_SHORTCUT": "Installa da Bower...",
+    "TITLE_QUICK_OPEN": "Installa da Bower",
+    "TITLE_CONFIG": "Configurazione",
+    "TITLE_DEPENDENCIES": "Dipendenze",
+    "TITLE_SETTINGS": "Impostazioni Bower",
+    "TITLE_PROJECT_PACKAGES": "Pacchetti di progetti",
+    "BOWERRC_NOT_FOUND": ".bowerrc file non trovato!",
+    "BOWER_JSON_NOT_FOUND": "Bower.json file non trovato!",
+    "INSTALLED_PACKAGES_NOT_FOUND": "Ancora nessun pacchetto installato",
+    "CREATE": "Crea",
+    "RELOAD": "Ricarica",
+    "DELETE": "Cancella",
+    "STATUS_BOWER_LOADING": "Caricamento registro Bower",
+    "STATUS_BOWER_READY": "Bower registro pronto",
+    "STATUS_BOWER_NOT_LOADED": "Impossibile caricare il registro Bower",
+    "STATUS_INSTALLING_PKG": "Installazione {0} ...",
+    "STATUS_PKG_INSTALLED": "Installato {0}",
+    "STATUS_ERROR_INSTALLING": "Errore installazione {0}",
+    "TEXT_CLOSE": "Chiudi",
+    "TEXT_CANCEL": "Cancella",
+    "TEXT_SAVE": "Salva",
+    "TEXT_RELOAD_DEFAULTS": "Ricarica predefinito",
+    "TEXT_SETTINGS_RELOAD_TIME": "Il tempo per recuperare e ricaricare il Registro di Bower",
+    "TEXT_SETTINGS_RELOAD_TIME_UNIT": "(m)",
+    "TEXT_SETTINGS_SAVE_PACKAGES": "Salva i pacchetti usando QuickInstall a bower.json",
+    "GIT_NOT_FOUND_TITLE": "Git non é stato trovato nel tuo sistema operativo",
+    "GIT_NOT_FOUND_DESCRIPTION": "L'estensione Brackets-bower ricerca per Git all'indirizzo di sistema PATH.\n\nBower richiede che Git sia installato, e configurato nella variabile di sistema PATH, \nto scarica e installa qualche pacchetto.\nBrackets-bower estensione potrebbe non funzionare correttamente.",
+    "COMMAND_INSTALL": "installla",
+    "COMMAND_PRUNE": "portare",
+    "COMMAND_UNINSTALL": "disintalla",
+    "COMMAND_UPDATE": "aggiorna",
+    "STATUS_EXECUTING_COMMAND": "Comandi in esecuzione \"{0}\"",
+    "STATUS_ERROR_EXECUTING_COMMAND": "Errore comando di esecuzione \"{0}\"",
+    "STATUS_SUCCESS_INSTALLING": "Pacchetti installati",
+    "STATUS_SUCCESS_EXECUTING_COMMAND": "Commando \"{0}\" eseguito",
+    "STATUS_NO_PACKAGES_INSTALLED": "Non c'è nessun pacchetto da installare",
+    "ERROR_RELOAD_TIME_VALUE": "* Il tempo deve essere maggiore di {0} minuti"
+});
